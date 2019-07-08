9c2a3303405cecb581c11bf247955bd3b32d12f5
Saad Ahmed <dropheaven@live.com>
2019-07-08 18:37:56 -0400

    clear local storage if tasks cannot be parsed. Alert user if task already exists and add delete button to task item.

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const form = document.querySelector('.task-form');
 const showButton = document.querySelector('.show-button');
 const tasksContainer = document.querySelector('.show-tasks');
 
+
 document.addEventListener('DOMContentLoaded', () => {
    const tasks = localStorage.getItem('tasks');
+
+   try {
+      JSON.parse(tasks);
+   } catch {
+      localStorage.clear();
+   }
+
+   // if a tasks key doesn't exist in local storage add an empty array to it
    if (!tasks) {
       localStorage.setItem('tasks', "[]");
    }
@@ -13,8 +22,17 @@ addEventListener('submit', e => {
    e.preventDefault();
    const task = e.target.elements[0].value;
    const tasks = JSON.parse(localStorage.getItem('tasks'));
-   tasks.push(task);
-   localStorage.setItem('tasks', JSON.stringify(tasks));
+
+   // checking if input isn't empty & input is unique
+   if (tasks.find(t => t.toLowerCase() === task.toLowerCase())) {
+      const alert = document.createElement('h3');
+      alert.textContent = 'task already exists';
+      tasksContainer.appendChild(alert);
+   } else {
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      e.target.elements[0].value = '';
+   }
 });
 
 showButton.addEventListener('click', () => {
@@ -23,7 +41,12 @@ showButton.addEventListener('click', () => {
 
    tasks.forEach(task => {
       const taskItem = document.createElement('li');
-      taskItem.innerText = task;
+      taskItem.textContent = task;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'delete';
+      taskItem.appendChild(deleteButton);
+
       tasksList.appendChild(taskItem);
    });
 
