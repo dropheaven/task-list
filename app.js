2dd8e6adf34739586042a771ef80ad878529246f
Saad Ahmed <dropheaven@live.com>
2019-07-08 17:48:25 -0400

    persist tasks in local storage

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const form = document.querySelector('.task-form');
 const showButton = document.querySelector('.show-button');
 const tasksContainer = document.querySelector('.show-tasks');
 
-// tasks array
-const tasks = [];
+document.addEventListener('DOMContentLoaded', () => {
+   const tasks = localStorage.getItem('tasks');
+   if (!tasks) {
+      localStorage.setItem('tasks', "[]");
+   }
+})
 
 addEventListener('submit', e => {
    e.preventDefault();
    const task = e.target.elements[0].value;
+   const tasks = JSON.parse(localStorage.getItem('tasks'));
    tasks.push(task);
+   localStorage.setItem('tasks', JSON.stringify(tasks));
 });
 
 showButton.addEventListener('click', () => {
    const tasksList = document.createElement('ol');
+   const tasks = JSON.parse(localStorage.getItem('tasks'));
 
    tasks.forEach(task => {
       const taskItem = document.createElement('li');
